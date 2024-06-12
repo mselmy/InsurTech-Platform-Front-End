1e3d184f5578a2fc850a6c1ef4874a3c60fd5ac7
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-06-12 15:46:08 +0300

    Project Inetializing

---
+++ src/app/features/admin-dashboard/components/scoped-shared-component-a/scoped-shared-component-a.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scoped-shared-component-a',
+  standalone: true,
+  imports: [],
+  templateUrl: './scoped-shared-component-a.component.html',
+  styleUrl: './scoped-shared-component-a.component.css'
+})
+export class ScopedSharedComponentAComponent {
+
+}
---
+++ src/app/shared/components/shared-button/shared-button/shared-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './shared-button.component.html',
+  styleUrl: './shared-button.component.css'
+})
+export class SharedButtonComponent {
+
+}
