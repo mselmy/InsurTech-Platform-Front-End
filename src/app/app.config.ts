c6af1544dd90f4b7ab7fc7e55ef256a8bbb98ae8
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-06-16 23:16:46 +0300

    implement insurance request modal

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from '../environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule), provideAnimationsAsync(),
+    { provide: 'BASE_URL', useValue: environment.baseUrl}, provideAnimationsAsync()
   ]
 };
--- src/app/core/services/registration-requests.service.ts
+++ src/app/core/services/registration-requests.service.ts
@@ -1,27 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { BASE_URL } from '../base-url';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationRequestsServiceService {
-  private baseUrl: string = 'http://localhost:5028/api';
+  
   constructor( private http:HttpClient) { }
 
   GetAll(){
-    return this.http.get(this.baseUrl + '/companies');
+    return this.http.get(BASE_URL + '/companies');
   }
 
   GetById(id: number){
-    return this.http.get(this.baseUrl + '/companies/' + id);
+    return this.http.get(BASE_URL + '/companies/' + id);
   }
 
   ApproveRequest(id: number){
-    return this.http.post(this.baseUrl + '/companies/ApproveCompany/' + id, {});
+    return this.http.post(BASE_URL + '/companies/ApproveCompany/' + id, {});
   }
 
   RejectRequest(id: number){
-    return this.http.post(this.baseUrl + '/companies/RejectCompany/' + id, {});
+    return this.http.post(BASE_URL + '/companies/RejectCompany/' + id, {});
   }
 }
