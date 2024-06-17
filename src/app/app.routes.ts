806671220997546391817d78f960963c114cc9d4
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-06-17 21:16:44 +0300

    chore: Update imports in app.component.ts and app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { AdminDashboardComponent } from './features/admin-dashboard/admin-dashbo
 import { RegistrationRequestsComponent } from './features/admin-dashboard/components/registration-requests/registration-requests.component';
 import { HomeComponent } from './home/home.component';
 import { QuestionsFormComponent } from './shared/components/questions-form/questions-form.component';
+import { AppLayoutComponent } from './layout/app.layout.component';
 
 export const routes: Routes = [
     {path: 'admin', component: AdminDashboardComponent, children: [
         {path: 'registration-requests', component: RegistrationRequestsComponent}
     ]
     },
+    {path: 'test', component: AppLayoutComponent},
     {path: 'home', component: HomeComponent},
     {path: 'form', component: QuestionsFormComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'}
