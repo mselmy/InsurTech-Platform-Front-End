91177970b401a7fe301ce9cf2f1e611a7564ff1d
Maryam <maryam.mohamed.elmasry112@gmail.com>
2024-06-19 03:31:14 +0300

    added Home Category

---
+++ src/app/Categories/sub-Homecategory/sub-category.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { CategoriesService } from '../../services/categories.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'Homecategory',
+  standalone: true,
+  imports: [TableModule, ButtonModule, CommonModule, FormsModule],
+  templateUrl: './sub-category.component.html',
+  styleUrl: './sub-category.component.css',
+})
+export class SubCategoryHomeComponent {
+
+  constructor(private router: Router, private categoriesService: CategoriesService) {}
+  data: any;
+
+  ngOnInit() {
+    this.categoriesService.getCategories('http://localhost:5028/api/HomeInsurance/GetHomeInsurance').subscribe((data) => {
+      this.data = data;
+      console.log(data);
+    });
+  }
+  goBack(): void {
+    this.router.navigate(['/Insurance']); 
+  }
+
+  
+}
