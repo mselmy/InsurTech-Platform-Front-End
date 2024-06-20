f5eacaa8071a1ceddd42a22dbd3646ccff102aad
Maryam <maryam.mohamed.elmasry112@gmail.com>
2024-06-20 06:11:36 +0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,13 @@ import { QuestionsFormComponent } from './features/apply-for-insurance/component
 import { AppLayoutComponent } from './features/admin-dashboard/layout/app.layout.component';
 import { RegisterComponent } from './register/register.component';
 import { ApplyForInsuranceComponent } from './features/apply-for-insurance/apply-for-insurance.component';
+import { DisplayComponent } from './Categories/display/display.component';
+import { SubCategoryHomeComponent } from './Categories/sub-Homecategory/sub-category.component';
+import { SubCategoryMotorComponent } from './Categories/sub-Motorcategory/sub-category.component';
+import { SubCategoryHealthComponent } from './Categories/sub-Healthcategory/sub-category.component';
+import { AppComponent } from './app.component';
+import { CompanyInsurancePlanComponent } from './Company/company-insurance-plan/company-insurance-plan.component';
+import { FAQComponent } from './faq/faq.component';
 
 export const routes: Routes = [
   {
@@ -20,4 +27,30 @@ export const routes: Routes = [
   { path: 'apply-for-insurance', component: ApplyForInsuranceComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: 'Insurance',
+    component: AppComponent,
+    children: [
+      { path: '', component: DisplayComponent },
+      { path: 'Health', component: SubCategoryHealthComponent },
+      { path: 'Home', component: SubCategoryHomeComponent },
+      { path: 'Motor', component: SubCategoryMotorComponent }
+    ]
+  },
+  {
+    path: 'CompanyInsurance',
+    component: AppComponent,
+    children: [
+      { path: '', component: CompanyInsurancePlanComponent },
+     
+    ]
+  },
+  {
+    path: 'FAQs',
+    component: AppComponent,
+    children: [
+      { path: '', component: FAQComponent },
+     
+    ]
+  },
 ];
