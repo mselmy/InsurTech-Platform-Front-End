8f91b49849d09fc5f0c610d1c0583f1341bdb319
EbrahimDawoud <ebrahim_EN30895331@f-eng.tanta.edu.eg>
2024-06-21 22:23:17 +0300

    fix: resolve conflicts and fixingsome bugs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000/api' // Replace with your actual API URL
+    apiUrl: 'http://localhost:5028/api' // Replace with your actual API URL
   };
