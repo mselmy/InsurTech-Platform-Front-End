573956ed9e91800141e1b3a9bf0bd20a905c342b
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-06-23 22:42:37 +0300

    fix: refactor apply for insurance

--- src/app/core/services/questions-form.service.ts
+++ src/app/core/services/questions-form.service.ts
@@ -2,57 +2,63 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { BASE_URL } from '../base-url';
 import { Question } from '../models/question';
+import { Answer } from '../types/Answer';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuestionsFormService {
   questionArr: any = [];
   questIndex: number = 0;
-  answers: { questionId: number, answer: string }[] = [];
+  answers: { questionId: number; answer: string }[] = [];
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  GetById(id: number){
-    return this.http.get<Question[]>(BASE_URL + '/Customers/GetQusetionsByCategory/' + id);
+  GetById(id: number) {
+    return this.http.get<Question[]>(
+      BASE_URL + '/Customers/GetQusetionsByCategory/' + id
+    );
   }
 
-  CreateInsurancePlanRequest(request: any){
-    return this.http.post(BASE_URL + '/Customers/requestInsurancePlan', request);
+  CreateInsurancePlanRequest(request: any) {
+    return this.http.post(
+      BASE_URL + '/Customers/requestInsurancePlan',
+      request
+    );
   }
-  GetQuestiosArr(id:number){
+  GetQuestiosArr(id: number) {
     this.GetById(id).subscribe((data) => {
       this.questionArr = data;
       // console.log(this.questionArr, 'questionArr from the service ya m3rs');
     });
     // console.log(this.questionArr, 'questionArr after the service ya m3rs');
   }
-  getQuestion(){
+  getQuestion() {
     return this.questionArr[this.questIndex];
   }
-  NextQuestion(){
-   if(this.questIndex < this.questionArr.length - 1){
-     this.questIndex++;
-   }
-
+  NextQuestion() {
+    if (this.questIndex < this.questionArr.length - 1) {
+      this.questIndex++;
+    }
   }
-  PreviousQuestion()
-  {
-    if(this.questIndex > 0){
+  PreviousQuestion() {
+    if (this.questIndex > 0) {
       this.questIndex--;
     }
-
   }
-  AddAnswer(answer: string){
-   if(this.answers.length == this.questionArr.length )
-    {
+  AddAnswer(answer: Answer) {
+    //we can find if the there is an answer for this question
+    const index = this.answers.findIndex(
+      (x) => x.questionId == answer.questionId
+    );
+    if (index != -1) {
+      this.answers[index] = answer;
       return;
     }
-    
-    this.answers.push({questionId: this.questionArr[this.questIndex].id, answer: answer});
+
+    this.answers.push(answer);
   }
-  GetAnswers(){
+  GetAnswers() {
     return this.answers;
   }
-
 }
--- src/app/features/apply-for-insurance-v2/questions-list/questions-list.component.ts
+++ src/app/features/apply-for-insurance-v2/questions-list/questions-list.component.ts
@@ -1,32 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { QuestionsFormService } from '../../../core/services/questions-form.service';
 import { QuestionCardComponent } from '../question-card/question-card.component';
 import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-questions-list',
   standalone: true,
   imports: [QuestionCardComponent, CommonModule],
   templateUrl: './questions-list.component.html',
-  styleUrl: './questions-list.component.css'
+  styleUrl: './questions-list.component.css',
 })
-export class QuestionsListComponent implements OnInit{
-
-  catId : number = 0;
+export class QuestionsListComponent implements OnInit, OnDestroy {
+  routeSub: Subscription | null = null;
+  dataSub: Subscription | null = null;
+  catId: number = 0;
   questionArr: any = [];
 
- constructor(private route: ActivatedRoute, public QuestionService: QuestionsFormService ) {}
-  
- ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.catId = params['id'];
-      // fill the questionArr with the questions of the category
-      this.QuestionService.GetQuestiosArr(this.catId);
-    });
-  
+  constructor(
+    private route: ActivatedRoute,
+    public QuestionService: QuestionsFormService
+  ) {}
 
-  
+  ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe((params) => {
+      this.catId = params['id'];
+
+      this.dataSub = this.QuestionService.GetById(this.catId).subscribe(
+        (data) => {
+          this.questionArr = data;
+          this.QuestionService.questionArr = data;
+        }
+      );
+    });
   }
-  
+  ngOnDestroy(): void {
+    this.routeSub?.unsubscribe();
+    this.dataSub?.unsubscribe();
+  }
 }
