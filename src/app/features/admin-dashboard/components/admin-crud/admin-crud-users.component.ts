8fe5819f3fa3e4d356eb0a49bfec15483378d796
Hussien <se7s_eleraqi@hotmail.com>
2024-06-24 13:03:12 +0300

    searchBar

--- src/app/features/admin-dashboard/components/admin-crud/admin-crud-users.component.ts
+++ src/app/features/admin-dashboard/components/admin-crud/admin-crud-users.component.ts
@@ -2,22 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../layout/service/crud-user.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-admin-crud-users',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './admin-crud-users.component.html',
-  styleUrl: './admin-crud-users.component.css',
+  styleUrls: ['./admin-crud-users.component.css'],
 })
 export class AdminCrudUsersComponent implements OnInit {
   users: any[] = [];
+  filteredUsers: any[] = [];
+  searchTerm: string = '';
+
   constructor(private userService: UserService) {}
+
   ngOnInit() {
     this.userService.getUsers().subscribe((data) => {
       this.users = data;
+      this.filteredUsers = data; // Initialize filteredUsers
     });
   }
+
   getUserType(userType: number) {
     return userType === 1 ? 'company' : 'customer';
+  }
+
+  filterUsers() {
+    if (!this.searchTerm) {
+      this.filteredUsers = this.users;
+    } else {
+      this.filteredUsers = this.users.filter(
+        (user) =>
+          user.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+          user.userName.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+          user.email.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+          user.phoneNumber.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
 }
