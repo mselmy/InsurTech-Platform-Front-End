3d5ccd574af67e2f1400e1c624dd2b10b3f3d7e5
Mohamed Selmy <40612774+mselmy@users.noreply.github.com>
2024-06-24 14:36:18 +0300

    Update admin-dashboard.routes.ts

--- src/app/features/admin-dashboard/admin-dashboard.routes.ts
+++ src/app/features/admin-dashboard/admin-dashboard.routes.ts
@@ -6,25 +6,11 @@ import { authAdminGuard } from '../../core/guards/authAdmin.guard';
 import { AdminCrudUsersComponent } from './components/admin-crud/admin-crud-users.component';
 
 export const adminDashboardRoutes: Routes = [
-  {
-    path: '',
-    component: AppLayoutComponent,
-    children: [
-      {
-        path: 'registration-requests',
-        component: RegistrationRequestsComponent,
-        canActivate: [authAdminGuard],
-      },
-      {
-        path: 'article',
-        component: ArticleComponent,
-        canActivate: [authAdminGuard],
-      },
-      {
-        path: 'crud',
-        component: AdminCrudUsersComponent,
-        canActivate: [authAdminGuard],
-      },
-    ],
-  },
+  { path: '', component: AppLayoutComponent,
+        children: [
+            { path: 'registration-requests', component: RegistrationRequestsComponent, canActivate: [authAdminGuard]},
+            { path: 'article', component: ArticleComponent, canActivate: [authAdminGuard] },
+            { path: 'crud', component: AdminCrudUsersComponent, canActivate: [authAdminGuard] }
+        ]
+    },
 ];
