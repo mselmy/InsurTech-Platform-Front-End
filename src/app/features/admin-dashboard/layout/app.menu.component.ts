609ae1f81a93840b3b29275ac6b4922229500056
Mohamed Selmy <40612774+mselmy@users.noreply.github.com>
2024-06-24 14:38:48 +0300

    Update app.menu.component.ts

--- src/app/features/admin-dashboard/layout/app.menu.component.ts
+++ src/app/features/admin-dashboard/layout/app.menu.component.ts
@@ -12,47 +12,26 @@ export class AppMenuComponent implements OnInit {
   constructor(public layoutService: LayoutService) {}
 
   ngOnInit() {
-    this.model = [
-      {
-        label: 'Home',
-        items: [
-          {
-            label: 'Dashboard',
-            icon: 'pi pi-fw pi-home',
-            routerLink: ['/'],
-          },
-        ],
-      },
-      {
-        label: 'Company',
-        items: [
-          {
-            label: 'Registration Request',
-            icon: 'pi pi-fw pi-user',
-            routerLink: ['/admin/registration-requests'],
-          },
-        ],
-      },
-      {
-        label: 'User',
-        items: [
-          {
-            label: 'Article',
-            icon: 'pi pi-fw pi-book',
-            routerLink: ['/admin/article'],
-          },
-        ],
-      },
-      {
-        label: 'Users Detailes',
-        items: [
-          {
-            label: 'DETAILES',
-            icon: 'pi pi-fw pi-database',
-            routerLink: ['/admin/crud'],
-          },
-        ],
-      },
-    ];
-  }
+        this.model = [
+            {
+                label: 'Home',
+                items: [
+                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
+                ]
+            },
+            {
+                label: 'Company',
+                items: [
+                    { label: 'Registration Request', icon: 'pi pi-fw pi-user', routerLink: ['/admin/registration-requests'] }
+                ]
+            },
+            {
+                label: 'User',
+                items: [
+                    { label: 'Article', icon: 'pi pi-fw pi-book', routerLink: ['/admin/article'] },
+                    { label: 'Details', icon: 'pi pi-fw pi-database', routerLink: ['/admin/crud'] }
+                ]
+            }
+        ];
+    }
 }
