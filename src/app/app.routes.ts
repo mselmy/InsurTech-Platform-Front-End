7c2660a14201b8fdad7c87e310c3a3444a6d8ba4
HabibaShata <156047923+HabibaShata@users.noreply.github.com>
2024-06-25 11:40:05 +0300

    Update Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,15 +10,20 @@ import { DisplayComponent } from './features/categories/display/display.componen
 import { SubCategoryHealthComponent } from './features/categories/sub-Healthcategory/sub-category.component';
 import { SubCategoryMotorComponent } from './features/categories/sub-Motorcategory/sub-category.component';
 import { AppComponent } from './app.component';
-import {LoginComponent} from './features/Account/login/login.component'
-import {ForgetPasswordComponent} from './features/Account/forget-password/forget-password.component'
-import{ResetpasswordComponent} from './features/Account/resetpassword/resetpassword.component'
+import { LoginComponent } from './features/Account/login/login.component'
+import { ForgetPasswordComponent } from './features/Account/forget-password/forget-password.component'
+import { ResetpasswordComponent } from './features/Account/resetpassword/resetpassword.component'
 import { ApplyForInsuranceV2Component } from './features/apply-for-insurance-v2/apply-for-insurance-v2.component';
 import { QuestionsListComponent } from './features/apply-for-insurance-v2/questions-list/questions-list.component';
 import { authAdminGuard } from './core/guards/authAdmin.guard';
 import { HomeComponent } from './features/home/home.component';
 import { CompanyInsurancePlanComponent } from './features/company/company-insurance-plan/company-insurance-plan.component';
 import { SubCategoryHomeComponent } from './features/categories/sub-Homecategory/sub-category.component';
+import { CompanyRequestComponent } from './features/company-request/Component/requests/company-request.component';
+import { InsurancecollectionComponent } from './features/company-dashboard/Componants/insurancecollection/insurancecollection.component';
+import { HomeinsuranceComponent } from './features/company-dashboard/Componants/Homeinsurance/homeinsurance/homeinsurance.component';
+import { HealthinsuranceComponent } from './features/company-dashboard/Componants/Helathinsurance/healthinsurance/healthinsurance.component';
+import { MotorinsuranceComponent } from './features/company-dashboard/Componants/Motorinsurance/motorinsurance/motorinsurance.component';
 
 
 export const routes: Routes = [
@@ -28,7 +33,7 @@ export const routes: Routes = [
       import('./features/admin-dashboard/layout/app.layout.module').then(
         (m) => m.AppLayoutModule
       ),
-      canActivate: [authAdminGuard]
+    canActivate: [authAdminGuard]
   },
   { path: 'apply-for-insurance', component: ApplyForInsuranceComponent },
   { path: 'register', component: RegisterComponent },
@@ -37,34 +42,35 @@ export const routes: Routes = [
   { path: 'login/:authorization', component: LoginComponent },
   { path: 'forgetpassword', component: ForgetPasswordComponent },
   { path: 'resetpassword', component: ResetpasswordComponent },
-  { path: 'allarticles', component: AllarticlesComponent },
-  { path: 'homearticle', component: HomearticlesComponent },
+  {
+    path: 'request', component: CompanyRequestComponent,
+  },
   {
     path: 'Insurance',
     component: AppComponent,
     children: [
+      { path: 'Health', component: SubCategoryHealthComponent },
+      { path: 'Home', component: SubCategoryHomeComponent },
+      { path: 'Motor', component: SubCategoryMotorComponent },
       { path: '', component: DisplayComponent },
-      { path: 'health', component: SubCategoryHealthComponent },
-      { path: 'home', component: SubCategoryHomeComponent },
-      { path: 'motor', component: SubCategoryMotorComponent },
     ],
   },
   {
-    path: 'companyinsurance',
-    component: CompanyInsurancePlanComponent,
+    path: 'CompanyInsurance',
+    component: AppComponent,
     children: [{ path: '', component: CompanyInsurancePlanComponent }],
   },
   {
-    path:'dashboard',component:companydashbordcomponant,
-  },
+    path: 'dashboard', component: companydashbordcomponant,
 
-  { path: 'edithealthinsurance/:id', component:EditHealthInsurancePlanComponent},
-  { path: 'edithomeinsurance/:id', component:EdithomeinsuranceComponent},
-  { path: 'editmotorinsurance/:id', component:EditmotorinsuranceComponent },
-  {path:"HomePage",component:HomeComponent},
+  },
+  { path: 'edithealthinsurance/:id', component: EditHealthInsurancePlanComponent },
+  { path: 'edithomeinsurance/:id', component: EdithomeinsuranceComponent },
+  { path: 'editmotorinsurance/:id', component: EditmotorinsuranceComponent },
+  { path: "HomePage", component: HomeComponent },
   {
-    path:"apply-for-insuranceV2",
-    component:ApplyForInsuranceV2Component,
+    path: "apply-for-insuranceV2",
+    component: ApplyForInsuranceV2Component,
     // children:[
     //   {
     //     path:"Question/:id",
@@ -73,8 +79,23 @@ export const routes: Routes = [
     // ]
   },
   {
-    path:"questions-list/:id",
-    component:QuestionsListComponent
+    path: "questions-list/:id",
+    component: QuestionsListComponent
   },
+  {
+    path: 'insurancecollection', component: InsurancecollectionComponent,
+    children: [
+      {
+        path: 'homeinsurance', component: HomeinsuranceComponent
+      },
+      {
+        path: 'healthinsurance', component: HealthinsuranceComponent
+      },
+      {
+        path: 'motorinsurance', component: MotorinsuranceComponent
+      }
+    ]
+  },
+  
   { path: '', component: HomeComponent, pathMatch: 'full' }
 ];
