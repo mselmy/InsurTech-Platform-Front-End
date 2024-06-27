21355b0c9fdcbd6f70f89b624948ab0deb8ff416
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-06-27 12:21:57 +0300

    chore: Update environment file imports and configurations

--- src/app/features/admin-dashboard/layout/app.layout.component.ts
+++ src/app/features/admin-dashboard/layout/app.layout.component.ts
@@ -7,6 +7,7 @@ import { AppTopBarComponent } from './app.topbar.component';
 
 @Component({
     selector: 'app-layout',
+    styleUrls: ['./app.layout.component.css'],
     templateUrl: './app.layout.component.html',
 })
 export class AppLayoutComponent implements OnDestroy {
--- src/app/features/apply-for-insurance-v2/categories/categories.component.ts
+++ src/app/features/apply-for-insurance-v2/categories/categories.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [RouterModule, RouterLink],
+  imports: [RouterModule, RouterLink, HeaderComponent],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
