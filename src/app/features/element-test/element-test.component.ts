1f1a46aaaf32bb6857386d171bff98e5b0bc68ed
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-06-28 22:44:04 +0300

    feat: Add ngx-lottie animation to element-test component

--- src/app/features/element-test/element-test.component.ts
+++ src/app/features/element-test/element-test.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-element-test',
   standalone: true,
-  imports: [],
+  imports: [LottieComponent],
   templateUrl: './element-test.component.html',
   styleUrl: './element-test.component.css'
 })
 export class ElementTestComponent {
-
+  options: AnimationOptions = {
+    path: 'assets/animations/Car Insurance 2.json',
+  };
 }
