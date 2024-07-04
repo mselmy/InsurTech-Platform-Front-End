ccf40bcea1f495b919bb5a756231d1f3b6062ac4
nayira411 <nayiratalat1@gmail.com>
2024-07-04 19:47:20 +0300

    add search

--- src/app/features/company-dashboard/Componants/users-company/users-company.component.ts
+++ src/app/features/company-dashboard/Componants/users-company/users-company.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CompanyUsers } from '../../Model/company/CompanyUser';
 import { CompanyService } from '../../Services/CompanyServices/company.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-users-company',
   standalone: true,
-  imports:[CommonModule],
+  imports:[CommonModule,FormsModule],
   templateUrl: './users-company.component.html',
   styleUrls: ['./users-company.component.css']
 })
