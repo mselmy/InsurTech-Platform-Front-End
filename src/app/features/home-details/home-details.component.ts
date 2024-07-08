a03653040b5479696d7a381e4285cac60fb872c0
nayira411 <nayiratalat1@gmail.com>
2024-07-08 06:22:15 +0300

    add static files for each category

--- src/app/features/home-details/home-details.component.ts
+++ src/app/features/home-details/home-details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-home-details',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home-details.component.html',
   styleUrl: './home-details.component.css'
 })
