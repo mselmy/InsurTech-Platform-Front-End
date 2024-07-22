3f76f2a3caaebe98c1003de343712bbcd901e45b
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-07-22 09:29:57 +0300

    Revert "Home Insurance Questions"
    This reverts commit 7cf778784105186527090b6609ab5dd607a322cd.

--- src/app/features/apply-for-insurance-v2/question-card/question-card.component.ts
+++ src/app/features/apply-for-insurance-v2/question-card/question-card.component.ts
@@ -1,149 +1,50 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, input } from '@angular/core';
+import { QuestionsFormService } from '../../../core/services/questions-form.service';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { Question } from '../../../core/models/question';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { ListboxModule } from 'primeng/listbox';
-import { QuestionsFormService } from '../../../core/services/questions-form.service';
 
 @Component({
   selector: 'app-question-card',
   standalone: true,
-  imports: [CommonModule, FormsModule, SelectButtonModule, ListboxModule],
+  imports: [FormsModule],
   templateUrl: './question-card.component.html',
-  styleUrls: ['./question-card.component.css'],
+  styleUrl: './question-card.component.css',
 })
-export class QuestionCardComponent implements OnChanges {
-  @Input() question: Question | null = null;
-  @Input() totalQuestions: number = 0;
-  @Input() currentIndex: number = 0;
+export class QuestionCardComponent {
+  @Input() question: null | Question = null;
   answer: string = '';
-  selectedValue: any; // For SelectButton
-  selectedCity: string = ''; // For Listbox
-  isPositiveNumberValid: boolean = true; // For positive number validation
-
-  stateOptions: { label: string; value: any }[] = [
-    { label: 'house', value: 'House' },
-    { label: 'tenant', value: 'Tenant' },
-    // Add more options as needed
-  ];
-
-  cities: { name: string; code: string }[] = [
-    { name: 'Alexandria', code: 'AL' },
-    { name: 'Aswan', code: 'Aswan' },
-    { name: 'Assiut', code: 'AST' },
-    { name: 'Al-Sharqia', code: 'SH' },
-    { name: 'Beheira', code: 'BH' },
-    { name: 'Beni Suef', code: 'BS' },
-    { name: 'Cairo', code: 'CAI' },
-    { name: 'Dakhlia', code: 'Dk' },
-    { name: 'Demitta', code: 'dm' },
-    { name: 'Fayoum', code: 'FY' },
-    { name: 'Gharbia', code: 'GH' },
-    { name: 'Giza', code: 'GZ' },
-    { name: 'Ismailia', code: 'IS' },
-    { name: 'KafrElshikh', code: 'Kafr' },
-    { name: 'Matrouh', code: 'MTR' },
-    { name: 'Minya', code: 'MIN' },
-    { name: 'Monofia', code: 'MON' },
-    { name: 'New Valley', code: 'NV' },
-    { name: 'North Sinai', code: 'NS' },
-    { name: 'Port Said', code: 'PS' },
-    { name: 'Qualyubia', code: 'QB' },
-    { name: 'Qena', code: 'QN' },
-    { name: 'Red Sea', code: 'RS' },
-    { name: 'Sohag', code: 'SG' },
-    { name: 'South Sinai', code: 'SS' },
-    { name: 'Suez', code: 'SZ' },
-    { name: 'Luxor', code: 'luxor' },
-    // Add more cities as needed
-  ];
-
   constructor(
     public questionService: QuestionsFormService,
-    private router: Router
+    private route: Router
   ) {}
-
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['currentIndex']) {
-      console.log('Current Index Changed:', this.currentIndex);
-    }
-    if (changes['question']) {
-      this.question = changes['question'].currentValue;
-    }
+    console.log('changes', changes);
+    this.question = changes['question'].currentValue;
   }
-
-  isNextButtonDisabled(): boolean {
-    if (this.currentIndex === 0 && this.selectedValue === undefined) {
-      return true;
-    }
-    if (this.currentIndex === 1 && this.selectedCity === '') {
-      return true;
-    }
-    if (this.currentIndex === 2 && this.answer === '') {
-      return true;
-    }
-    if (
-      this.currentIndex === this.totalQuestions - 1 &&
-      (this.answer === '' || !this.isPositiveNumberValid)
-    ) {
-      return true;
-    }
-    return false;
-  }
-
   NextQuestion() {
     if (
-      this.questionService.questIndex ===
+      this.questionService.questIndex ==
       this.questionService.questionArr.length - 1
     ) {
-      this.router.navigate(['insurancePlans']);
+      this.route.navigate(['insurancePlans']);
       console.log('answers', this.questionService.GetAnswers());
-    } else {
-      this.questionService.NextQuestion();
-      this.answer = '';
-      this.selectedValue = undefined;
-      this.selectedCity = '';
     }
+    this.questionService.NextQuestion();
+    this.answer = '';
   }
-
   PreviousQuestion() {
     this.questionService.PreviousQuestion();
     this.answer = '';
-    this.selectedValue = undefined;
-    this.selectedCity = '';
   }
-
   AddAnswer() {
-    if (this.currentIndex === 0 && this.selectedValue !== undefined) {
-      this.questionService.AddAnswer({
-        questionId: this.question?.id || 0,
-        answer: this.selectedValue,
-      });
+    if (this.answer == '' || this.question == null) {
+      return;
     }
-    if (this.currentIndex === 1 && this.selectedCity) {
-      this.questionService.AddAnswer({
-        questionId: this.question?.id || 0,
-        answer: this.selectedCity,
-      });
-    }
-    if (this.currentIndex === 2 && this.answer !== '') {
-      this.questionService.AddAnswer({
-        questionId: this.question?.id || 0,
-        answer: this.answer,
-      });
-    }
-    if (this.currentIndex === this.totalQuestions - 1 && this.answer !== '') {
-      this.questionService.AddAnswer({
-        questionId: this.question?.id || 0,
-        answer: this.answer,
-      });
-    }
+    this.questionService.AddAnswer({
+      questionId: this.question.id,
+      answer: this.answer,
+    });
     this.NextQuestion();
-  }
-
-  validatePositiveNumber() {
-    this.isPositiveNumberValid = parseFloat(this.answer) > 0;
   }
 }
